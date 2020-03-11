b62220afbf252cab9ef5d3c597a2699daccc113f
Francis omondi <franisomondi17@gmail.com>
2020-03-11 17:02:12 +0300

    added my first route and a model(postmodel)

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
+const mongoose = require ('mongoose');
+require('dotenv/config');
+
+
+//importing routes
+postsRouter = require('./routes/post');
+
+//DB CONNECTION
+mongoose.connect(process.env.DB_CONNECTION,
+    { useNewUrlParser: true,
+    useUnifiedTopology: true  },
+    ()=> console.log('connected to db'));
 
 
 //routes
 app.get('/', (req,res)=>{
  res.send('hello world');
 });
+
+app.use('/posts', postsRouter);
 
 //listen
 app.listen(3000, console.log('server started on port 3000'));
